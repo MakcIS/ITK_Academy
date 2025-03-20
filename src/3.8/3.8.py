797b371a66cd6df79d77b7a0ec489b290fff401b
Maksim Yakushev <makcis@list.ru>
2025-03-20 19:36:38 +0700

    3.8 update

--- src/3.8/3.8.py
+++ src/3.8/3.8.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import os
 
 import aiohttp
 
@@ -12,20 +13,27 @@ urls = [
 semaphore = asyncio.Semaphore(5)
 
 
-async def fetch_urls(urls: list[str], file_path: str):
+async def fetch_url(session: aiohttp.ClientSession, url: str):
     async with semaphore:
-        async with aiohttp.ClientSession() as session:
-            for url in urls:
-                try:
-                    async with session.get(url) as response:
-                        result = {"url": url, "status_code": response.status}
-                except aiohttp.ClientConnectorError:
-                    result = {"url": url, "status_code": 0}
-                except asyncio.TimeoutError:
-                    result = {"url": url, "status_code": 1}
-                with open(file_path, "a") as file:
-                    json.dump(result, file)
+        try:
+            async with session.get(url) as response:
+                result = {"url": url, "status_code": response.status}
+        except aiohttp.ClientConnectorError:
+            result = {"url": url, "status_code": 0}
+        except asyncio.TimeoutError:
+            result = {"url": url, "status_code": 1}
+        return result
+
+
+async def fetch_urls(urls: list[str], file_path: str):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_url(session, url) for url in urls]
+        data = await asyncio.gather(*tasks)
+
+        with open(file_path, "a") as file:
+            for unit in data:
+                json.dump(unit, file)
 
 
 if __name__ == "__main__":
-    asyncio.run(fetch_urls(urls, "./results.jsonl"))
+    asyncio.run(fetch_urls(urls, os.path.dirname(__file__) + "/results.jsonl"))
