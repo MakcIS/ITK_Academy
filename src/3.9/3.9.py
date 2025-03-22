ab51ae978747d8b5919fb778ffef670386245435
Maksim Yakushev <makcis@list.ru>
2025-03-22 10:00:56 +0700

    3.9 update

--- src/3.9/3.9.py
+++ src/3.9/3.9.py
@@ -2,38 +2,54 @@ import asyncio
 import json
 import os
 
+import aiofiles
 import aiohttp
 from aiohttp.client_exceptions import (
     ClientConnectionError,
     ClientError,
 )
 
-semaphore = asyncio.Semaphore(5)
-
 
 async def fetch_url(session: aiohttp.ClientSession, url: str):
-    async with semaphore:
-        try:
-            async with session.get(url) as responce:
-                if responce.status == 200:
-                    result = await responce.json()
-                    return {url: result}
-                else:
-                    return {url: None}
-        except (ClientError, ClientConnectionError, asyncio.TimeoutError):
-            return {url: None}
+    try:
+        async with session.get(url) as responce:
+            if responce.status == 200:
+                result = await responce.json()
+                return {url: result}
+            else:
+                return {url: None}
+    except (ClientError, ClientConnectionError, asyncio.TimeoutError):
+        return {url: None}
 
 
-async def fetch_urls(file_path):
-    async with aiohttp.ClientSession as session:
-        with (
-            open(file_path, "r") as file,
-            open(os.path.dirname(__file__) + "/result.jsonl", "a") as result_file,
-        ):
-            for url in file:
-                url = url.strip()
+async def producer(queue: asyncio.Queue, path: str):
+    async with aiofiles.open(path, "r") as file:
+        async for url in file:
+            url = url.strip()
+            await queue.put(url)
 
-                result = await fetch_url(session, url)
 
-                if result["url"] is not None:
-                    json.dump(result, result_file)
+async def worker(queue: asyncio.Queue, session: aiohttp.ClientSession):
+    while True:
+        url = await queue.get()
+        result = await fetch_url(session, url)
+
+        if result[url] is not None:
+            with open(os.path.dirname(__file__) + "/result.jsonl", "a") as result_file:
+                json.dump(result, result_file)
+        queue.task_done()
+
+
+async def fetch_urls(path):
+    queue = asyncio.Queue(maxsize=5)
+
+    async with aiohttp.ClientSession() as session:
+        _ = [asyncio.create_task(worker(queue, session)) for _ in range(5)]
+
+        await asyncio.create_task(producer(queue, path))
+
+        await queue.join()
+
+
+if __name__ == "__main__":
+    asyncio.run(fetch_urls(os.path.dirname(__file__) + "/urls.txt"))
